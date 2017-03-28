fd87040caa0837a5c70b5a847b94105fd8729637
Matt <matt.voget@gmail.com>
2017-03-27 20:44:32 -0600

    initial network test code

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -16,8 +16,17 @@ export class HomeComponent implements OnInit {
   user: User;
 
   private creatingFolder = false;
-
   private homeLoading = false;
+
+  private tested = false;
+
+  private testing = false;
+  private results = 'Click Test to see your results';
+  private imageAddr = 'https://upload.wikimedia.org/wikipedia/commons/2/2c/A_new_map_of_Great_Britain_according_to_the_newest_and_most_exact_observations_%288342715024%29.jpg'; 
+  private downloadSize = 107726177; //bytes
+  private endTime = 0;
+  private startTime = 0;
+  private download = new Image();
 
   constructor(
     private userSvc: UserService,
@@ -59,5 +68,40 @@ export class HomeComponent implements OnInit {
       this.user = null;
     });
   }
+
+  testNetwork(): void {
+
+    this.testing = true;
+    
+    let endTime = 0;
+    let startTime = 0;
+    let dlSize = this.downloadSize;
+    let results = this.results;
+    let msg = '';
+
+    this.download = new Image();
+
+    let onLoad = ():void => {
+      endTime = (new Date()).getTime();
+      let duration = (endTime - startTime) / 1000;
+      let bitsLoaded = dlSize * 8;
+      let speedBps:any = (bitsLoaded / duration).toFixed(2);
+      let speedKbps:any = (speedBps / 1024).toFixed(2);
+      let speedMbps = (speedKbps / 1024).toFixed(2);
+
+      let msg = "Your connection speed is: " + speedMbps + " Mbps";
+      console.log(msg);
+      this.results = msg;
+      this.testing = false;
+    }
+
+    this.download.onload = onLoad;
+
+    startTime = (new Date()).getTime();
+    var cacheBuster = "?nnn=" + startTime;
+    this.download.src = this.imageAddr + cacheBuster;
+
+  }
+  
 
 }
