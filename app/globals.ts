1a8a0578d4145e7090b5a49cbb2e1b92632e03b2
Matt <matt.voget@gmail.com>
2017-04-02 16:16:57 -0600

    changed sarlacc url

--- app/globals.ts
+++ app/globals.ts
@@ -1,4 +1,4 @@
 export class Globals {
   svc_domain: string = 'http://localhost:8090';
-  sarlacc_domain: string = 'http://sarlacc.mattvoget.com';
+  sarlacc_domain: string = 'http://sarlacc.voget.io';
 }
